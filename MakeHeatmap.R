9af2c982cd7d17b5224c2aed8db8e42dd4321d0e
Nathaniel Vacanti <nathaniel.vacanti@gmail.com>
2017-05-25 16:43:49 +0200

    fixed output directory address by removing extra /

--- MakeHeatmap.R
+++ MakeHeatmap.R
@@ -52,7 +52,7 @@ library(heatmap.plus)
 #Input where the heatmap is stored
 working_directory <- getwd()
 working_directory_up1 <- gsub('/[^/]*$','/',working_directory) #matches '/' followed by 0 or more characters other than '/' followed by the end of the string, and replaces with '/'
-HeatmapDirectory <- paste(working_directory_up1,'/output/',sep='')
+HeatmapDirectory <- paste(working_directory_up1,'output/',sep='')
 dir.create(HeatmapDirectory) #creates the directory for the output
 
 #Input data
