1b88393ef65c49d93858b2ed779d798b4b42a55c
Nathaniel Vacanti <Nate@NathanielVacanti.scilifelab.se>
2017-06-13 19:07:04 +0200

    MakeVolcanoPlot integrated

--- GetPs.R
+++ GetPs.R
@@ -11,39 +11,38 @@ GetPs <- function(group_order,n_gene,gene_name,DATA_long)
     rownames(sig_test_list[[1]]) <- gene_name
     colnames(sig_test_list[[1]]) <- c('ratio','p_val')
 
-    SigTestApplyReturn <- apply(gene_name[1:10],1,SigTest)
+    n_genes <- length(gene_name)
+    gene_name <- as.matrix(gene_name,nrow=n_genes)
 
-    #perform the t-tests
-    # for (i in 1:n_gene)
-    # {
-    #     if (i %% 100 == 0){print(i)}
-    #     current_gene <- gene_name[i]
-    #     indices_group_1 <- DATA_long[,'gene']==gene_name[i] & DATA_long[,'group']==group_order[1]
-    #     indices_group_2 <- DATA_long[,'gene']==gene_name[i] & DATA_long[,'group']==group_order[2]
-    #     value_group1 <- DATA_long[indices_group_1,'value']
-    #     value_group2 <- DATA_long[indices_group_2,'value']
-    #     t_test_result <- t.test(value_group1,value_group2,var.equal=TRUE)
-    #     p_val <- t_test_result[[3]]
-    #     ratio <- median(value_group1)/median(value_group2)
-    #     sig_test_list[[1]][current_gene,'ratio'] <- ratio
-    #     sig_test_list[[1]][current_gene,'p_val'] <- p_val
-    # }
+    sig_test_list <- list()
+    sig_test_list[[1]] <- data.frame(matrix(nrow=n_genes,ncol=2))
+    names(sig_test_list) <- group_test
+    rownames(sig_test_list[[1]]) <- gene_name[,1]
+    colnames(sig_test_list[[1]]) <- c('ratio','p_val')
 
+    start_time <- Sys.time()
+    SigTestApplyReturn <- t(apply(gene_name,1,SigTest,group_order=group_order,DATA_long=DATA_long))
+    end_time <- Sys.time()
+    print(paste('p_value and ratio calculation time:',' ',end_time-start_time,' ','minutes',sep=''))
+
+    rownames(SigTestApplyReturn) <- gene_name
+    colnames(SigTestApplyReturn) <- c('p_val','ratio')
+
+    sig_test_list[[1]][gene_name,'p_val'] <- SigTestApplyReturn[gene_name,'p_val']
+    sig_test_list[[1]][gene_name,'ratio'] <- SigTestApplyReturn[gene_name,'ratio']
     sig_test_list[[1]][,'log2_ratio'] <- log2(sig_test_list[[1]][,'ratio'])
     sig_test_list[[1]][,'nlog10_p'] <- -log10(sig_test_list[[1]][,'p_val'])
 
     return(sig_test_list)
 }
 
-SigTest(gene_name,groups,DATA_long)
+SigTest <- function(gene_name,group_order,DATA_long)
 {
-    indices_group_1 <- DATA_long[,'gene']==gene_name & DATA_long[,'group']==group_order[1]
-    indices_group_2 <- DATA_long[,'gene']==gene_name & DATA_long[,'group']==group_order[2]
-    value_group1 <- DATA_long[indices_group_1,'value']
-    value_group2 <- DATA_long[indices_group_2,'value']
+    value_group1 <- with(DATA_long,value[group==group_order[1] & gene==gene_name])
+    value_group2 <- with(DATA_long,value[group==group_order[2] & gene==gene_name])
     t_test_result <- t.test(value_group1,value_group2,var.equal=TRUE)
     p_val <- t_test_result[[3]]
     ratio <- median(value_group1)/median(value_group2)
-    SigTestReturn <- list(p_val,ratio)
-    return()
+    SigTestReturn <- c(p_val,ratio)
+    return(SigTestReturn)
 }
--- MakeVolcanoPlot.R
+++ MakeVolcanoPlot.R
@@ -1,30 +1,38 @@
-MakeVolcanoPlot <- function(data_location,select_groups,ColGroupsScheme,replicate_scheme,genes_to_label=NULL,select_rows=NULL,transformation='none')
-#data_file: the file (as a string) where the data to be plotted is
-  #the items grouped (e.g. patients) are across the columns and the items plotted (e.g. gene expression) are down the rows
-  #the rows and columns of the file have headings
-#volcano_directory: the directory where the output volcano plot is to be saved
-#group_designations_file: the file where the group designations of the columns are stored
-  #the file is tab delimited and has the column names across the top
-  #each row is a group naming scheme, for example breast cancer patients can be grouped based on the mRNA or protein profiles
-    #thus mRNA and protein groupings could be two rows or group naming schemes in this instance
-#group_designations_scheme: specifies the row used in the group_designations_file
-  #may be 'PAM50' or 'metabolite' for breast cancer patient groupings
-#select_groups: this is a list of 2 lists defined using list(c(),c()) or it is an array with two group names
-  #each member list contains the names of the groups as they are separated and grouped for analysis in the volcano plot
-  #for example, if you are comparing basal tumors to her2, normal like, luminal a, and luminal b, select_groups <- list(c('basal'),c('her2','normal like','luminal a','luminal b'))
-  #the second entry is the numerator, the first the denominator
-#Note: currently this function only plots -log10(p-value) vs. log2(ratio), where ratio is the median of the fist specified group division over the median of the second specified group division
-{
+MakeVolcanoPlot <- function(data_location,ColGroupsScheme=NULL,transformation=NULL,data=NULL,select_rows=NULL,select_groups=NULL,replicate_scheme=NULL)
+# data_location: a pathway to where the text file containing the data is stored, must have '/' at the end
+#    The data file must be named quantities.txt with the genes down the rows and sample names across the columns
+#    There must also be a group_key.txt file with the sample names across the columns and the grouping schemes down the rowss
+#        The group names within each grouping scheme must not match any of those of another grouping scheme
+#    There must also be a group_color_key.txt file with the group names across the columns and the colors beneath them
+#        All of the group names are listed across the column headers with no indication of their scheme membership (thus the names must be unique)
+#    These files must all be tab delimited
+# ColGroupsScheme: the name of the grouping scheme used, indicates which row to take from group_key.txt
+#    There can only be one for the MakeBoxPlot() function
+# transformation: This specifies how the rows should be transformed.
+#    Options include: 'log2', 'median_center_iqr_norm', and 'median_norm_log2_transform'
+# data: Is a data frame containing the data to be plotted if the data is passed as a data frame instead of through the quantities.txt file in data_location
+#    This option could use more testing
+# select_rows: These are the genes that you want to plot
+#    #If it is NULL, all genes are plotted
+# select_groups: This can be an array of the group names (from the provided ColGroupsScheme) that are to be plotted or a list of arrays of group names or NULL
+#    If it is an array of group names, those groups are the only ones plotted
+#    If it is a list of arrays of group names, groups in the same array are combined into a single group
+#    If it is NULL, all groups in the ColGroupsScheme are plotted
+# replicate_scheme: This specifies the grouping scheme that is used to specify groups of replicates
+#    This must NOT be a member of ColGroupsScheme, though it must be a grouping scheme defined in group_key.txt
+#        As such each member of this grouping scheme must also have colors specified in group_color_key.txt
+#    If this is specified, all members of a single group are treated as a single sample and the median values are used
 
+{
     #Stop the program if the replicate scheme is in the ColGroupsScheme
     #Stop the program if more than one ColGroupsScheme or replicate_scheme is specified
     CheckStop(1,parameters=list(ColGroupsScheme,replicate_scheme))
-    
+
     #Include pertinent libraries
-    library(ggplot2) #from ggplot2 package, allows the scatter plot to be made
+    library(ggplot2) #from ggplot2 package, allows the boxplot to be made
 
     #create the directory 'output' one level above the current directory for storing the heatmap
-    volcano_directory <- StoreHeatmap()
+    output_directory <- StoreHeatmap()
 
     #Input data
     if (!(class(data)=='data.frame')){data <- paste(data_location,'quantities.txt',sep='')}
@@ -32,120 +40,80 @@ MakeVolcanoPlot <- function(data_location,select_groups,ColGroupsScheme,replicat
     group_color_designations_file <- paste(data_location,'group_color_key.txt',sep='')
 
     #Import the data and only keep selected rows if specified
-    #Retrieve the corresponding column groupings and keep only those specified
-    #Also take the medians specified by the given replicate scheme
-    #There must be a ColGroupsScheme specified, the volcano plot compares two groups
     DATA <- OpenDataFile(data,select_rows)
+
+    #Retrieve the corresponding column groupings and keep only those specified
+    #Also take the medians if there is a replicate scheme provided (this will not work if the replicate scheme is the only grouping scheme used)
+    #There must be a ColGroupsScheme specified, as of now it cannot be FALSE
 
     #If the groups are packaged into lists, unpack them into a single array
         #This function does not affect the input if it is an array
     UnpackGroups_return <- UnpackGroups(select_groups)
-    select_groups <- UnpackGroups_return[[1]]
-    group_divisions <- UnpackGroups_return[[2]]
-
-    #Can specify a gene to label in red here:
-    genes_to_label_is_file <- FALSE
-    if (is.character(genes_to_label))
-    {
-        genes_to_label_is_file <- length(grep('.txt',genes_to_label)) > 0
-    }
-
-    if (genes_to_label_is_file)
-    {
-        genes_to_label_df <- read.table(file=genes_to_label,head=FALSE,check.names=FALSE,sep='\t')
-        genes_to_label_row <- genes_to_label_df[1,]
-        n_genes_to_label <- length(genes_to_label_row)
-        genes_to_label <- NULL
-        for (i in 1:n_genes_to_label) {genes_to_label <- c(genes_to_label,as.character(genes_to_label_row[1,i]))}
-     }
-
-    #store the patient and gene names
-    patient_names <- colnames(DATA)
-    gene_names <- rownames(DATA) #store the gene names
+    select_groups <- UnpackGroups_return[[1]] #this is now an array of the groups if it were input as a list
+    group_divisions <- UnpackGroups_return[[2]] #this is the original input if it was a list, NULL if the input was not a list
 
     #Match the group names to the samples by referencing the group_key
-    #This will also combine replicates if specified to do so, in volcano plots select groups indicates replicates!
+    ColGroupsScheme_as_input <- ColGroupsScheme
+    ColGroupsScheme <- c(ColGroupsScheme,replicate_scheme)
     RetrieveGroups_return <- RetrieveGroups(DATA,ColGroupsScheme,group_designations_file,group_color_designations_file,select_groups,replicate_scheme)
-    DATA <- RetrieveGroups_return[[1]]
-    groups_corresponding <- RetrieveGroups_return[[2]]
-    GroupColorMatrix <- RetrieveGroups_return[[3]]
-    COLOR_KEY <- RetrieveGroups_return[[4]]
-    group_values <- RetrieveGroups_return[[5]]
+    groups_corresponding <- RetrieveGroups_return[[1]]
+    GroupColorMatrix <- RetrieveGroups_return[[2]]
+    COLOR_KEY <- RetrieveGroups_return[[3]]
+    CheckStop(5,parameters=list(COLOR_KEY)) #makes sure each group name only has one color assignment
+
+    #See if all of the specified input groups are actually specified in group_key.txt file
+    CheckStop(2,parameters=list(select_groups,groups_corresponding))
+
+    #Select the groups that are considered for this box plot
+    inclusion_grouping_scheme <- ColGroupsScheme_as_input #It has to be the input ColGroupsScheme because a boxplot can only have one ColGroupsScheme
+    SelectGroups_return <- SelectGroups(select_groups,DATA,ColGroupsScheme,groups_corresponding,GroupColorMatrix,inclusion_grouping_scheme)
+    DATA <- SelectGroups_return[[1]]
+    groups_corresponding <- SelectGroups_return[[2]]
+    GroupColorMatrix <- SelectGroups_return[[3]]
 
     #If you are concatonating groups, name the new groups and replace all of the groups they map to those with names
     #Also get corresponding colors for those new groups by taking the color that maps to the first sub-group of each concatonated group
     #This function does not affect the input if group_divisions is NULL (i.e. select_groups was not passed as a list to the original function)
-    ConcatonateGroups_return <- ConcatonateGroups(group_divisions,groups_corresponding,GroupColorMatrix,COLOR_KEY)
+    concat_group_scheme <- ColGroupsScheme_as_input #It has to be the input ColGroupsScheme because a boxplot can only have one ColGroupsScheme
+    ConcatonateGroups_return <- ConcatonateGroups(group_divisions,groups_corresponding,GroupColorMatrix,COLOR_KEY,concat_group_scheme)
     groups_corresponding <- ConcatonateGroups_return[[1]]
-    #GroupColorMatrix <- ConcatonateGroups_return[[2]]
+    GroupColorMatrix <- ConcatonateGroups_return[[2]]
+    groups_concatonated <- ConcatonateGroups_return[[3]]
+    colors_concatonated <- ConcatonateGroups_return[[4]]
+    group_concationation <- is.list(group_divisions)
+
+    #return median of replicates if specified to do so
+    MedianGroup_return <- MedianGroup(DATA,groups_corresponding,GroupColorMatrix,replicate_scheme,ColGroupsScheme)
+    DATA <- MedianGroup_return[[1]]
+    groups_corresponding <- MedianGroup_return[[2]]
+    GroupColorMatrix <- MedianGroup_return[[3]]
 
     #Transform the data as specified
     DATA <- transform_data(DATA,transformation)
     gene_name <- rownames(DATA)
     n_gene <- length(gene_name)
 
-    #for each gene, find the median expression across each group division
-    for (i in gene_names)
+    #put the data in long data frame format for plotting
+    DATA_long <- FatToLongDF(DATA,groups_corresponding)
+
+    #specify the order in which the groups will be plotted and ensure they map to their corresponding colors
+    if (is.character(select_rows)){gene_name <- select_rows}
+    OrderGroups_return <- OrderGroups(select_groups,group_concationation,groups_corresponding,GroupColorMatrix,COLOR_KEY,groups_concatonated,colors_concatonated,gene_name,DATA_long)
+    DATA_long <- OrderGroups_return[[1]]
+    FillColors <- OrderGroups_return[[2]]
+    group_order <- OrderGroups_return[[3]]
+
+    #calculate p-values: currently can only do if there are 2 groups with an equal-variance t-test
+    #returns a data frame with the row naming the group pairwise comparison (e.g. basal-her2) and the column the parameter measured (e.g. glucose)
+    n_groups <- length(group_order)
+    sig_test_list <- list()
+    if (n_groups==2)
     {
-        #DATA[i,'ratio'] <- DATA[i,2]/DATA[i,1] #get a ratio of the mean expression
-        group_division_1_expression <- group_values[[1]][i,]
-        group_division_2_expression <- group_values[[2]][i,]
-        DATA[i,'p_value'] <- t.test(group_division_1_expression,group_division_2_expression,alternative='two.sided',mu=0,paired=FALSE,var.equal=TRUE,conf.level=0.95)$p.value #find the p-value of a t-test, mu is the difference between the two sets according to the null hypothesis
+        sig_test_list <- GetPs(group_order,n_gene,gene_name,DATA_long)
     }
 
-    groups = unique(groups_corresponding)
+    #Make the plot
+    vp <- assemble_volcano_plot(sig_test_list[[1]],output_directory)
 
-    #create columns for the processed variables that are plotted
-    group1 <- groups[1]
-    group2 <- groups[2]
-    DATA[,'ratio'] <- DATA[,group2]/DATA[,group1]
-    DATA[,'log2ratio'] <- log2(DATA$ratio)
-    DATA[,'nlog10p'] <- -log10(DATA$p)
-
-    #set the plot parameters
-    XData <- 'log2ratio' #the name of the column of the data frame to be plotted
-    YData <- 'nlog10p' #the name of the row of the data frame to be plotted
-    XAxisLimits <- 1.1*c(min(DATA$log2ratio),max(DATA$log2ratio))
-    YAxisLimits <- 1.1*c(0,max(DATA$nlog10p))
-    AxisLabels <- c('Log2 Ratio','-Log10(p-value)')
-    PlotDimensions <- c(4,4)
-
-    ScatterPlot <-  ggplot(DATA,aes_string(x=XData,y=YData)) +
-        geom_point(color='black') + #specifying one color may remove the legend, May be necessary to make this a string when considering groups
-        annotate('point',DATA[genes_to_label,'log2ratio'],DATA[genes_to_label,'nlog10p'],col='red') + #points to highlight in red on the volcano plot, x-points first, then y-points
-        annotate('text',DATA[genes_to_label,'log2ratio'],DATA[genes_to_label,'nlog10p'],col='#0088B2',label=rownames(DATA[genes_to_label,]),size=0.75) + #points to highlight in red on the volcano plot, x-points first, then y-points
-        coord_cartesian(xlim=XAxisLimits,ylim=YAxisLimits,expand = 0) + #expand=0 removes extra space before and after first and last tick marks
-        theme(axis.text.y=element_text(color='black',size=10)) +
-        theme(axis.ticks.y=element_line(colour='black',size=1)) +
-        theme(axis.ticks.x=element_line(colour='black',size=1)) +
-        theme(axis.text.x=element_text(color='black',size=10)) +
-        theme(axis.title.y=element_text(color='black',vjust=1,size=10)) +
-        theme(axis.title.x=element_text(color='black',vjust=0,size=10)) +
-        theme(panel.background=element_rect(fill=NA)) +
-        theme(axis.line.x=element_line(colour='black',size=1,linetype='solid')) +
-        theme(axis.line.y=element_line(colour='black',size=1,linetype='solid')) +
-        theme(panel.grid.minor=element_blank(), panel.grid.major=element_blank()) +#removes gridlines
-        theme(legend.title=element_blank()) +
-        theme(legend.key=element_rect(fill=NA)) +
-        theme(legend.text = element_text(colour="black", size = 6)) +
-        #theme(legend.position=LegendPosition) +
-        theme(legend.key.size = unit(0.3, "cm")) + #Space between legend symbols and text, maybe?
-        theme(legend.background = element_rect(fill="transparent",linetype = 0,colour = 'transparent')) +
-        labs(x=AxisLabels[1]) +
-        labs(y=AxisLabels[2])
-
-    print('work!') #There needs to be something in between ggplot call and ggsave call inside a function. This is some sort of bug in ggplot/ggsave
-
-    ggsave(paste(volcano_directory,'volcano.pdf',sep=''), width = PlotDimensions[1], height = PlotDimensions[2], dpi = 120)
-
-    return(DATA)
-}
-
-#############################################################################
-#Supporting Functions
-
-#Function to return TRUE if the row does not have any NAs
-NoNA <- function(vector)
-{
-    NoNA <- !is.na(sum(vector))
+    return(sig_test_list)
 }
