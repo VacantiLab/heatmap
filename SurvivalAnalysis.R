3d84c307aa80f41eca3f08cc6b3bbd9281b799cd
Nathaniel Vacanti <VacantiLab@users.noreply.github.com>
2017-07-21 12:17:14 +0200

    added comments to SurvivalAnalysis

--- SurvivalAnalysis.R
+++ SurvivalAnalysis.R
@@ -1,4 +1,21 @@
 SurvivalAnalysis <- function()
+# This function is currently being modified
+# This function analyzes patient survival and gene expression data over different tumor types
+# The data contained for each tumor type are stored in separate directories from the other tumor types
+#     Each tumor-type directory contains a text file with gene expression data and separate text file with patient survival data
+# Function design:
+#     This function loops through the tumor-types by looping through the tumor-type directories
+#     Once in a tumor-type directory, the function loops through the genes whose expressions were measured in the tumor-type study
+#     For each gene, a two parameter Cox survival model is fit, where patient age and expression of the gene are the Cox parameters
+#     For each gene within the tumor-type, a hazard coefficient along with it's 95% CI are calculated
+#     If the hazard coefficient is above 1 without the 95% CI overlapping 1, the gene is considered a risk factor and the variable risk_factor is given a value of 1
+#     If the hazard coefficient is below 1 without the 95% CI overlapping 1, the gene is considered a benefitial and the variable risk_factor is given a value of -1
+#     If the 95% CI overlaps 1, the gene is considered tumor neutral and the variable risk_factor is given a value of 0
+#     Once the nested loops through tumor-type and genes are completed, each gene for each tumor-type will have a risk_factor variable associated with it
+#     This will be stored in a data frame
+#          Euclidian distance hierarchical clustering of the genes will allow visualization of which genes act in concert as risk or survival factors
+#          Similar clustering of the tumors will allow visualization of which tumors share common risk and survival factor profiles
+
 {
     #load requisite libraries
     library(ggplot2)
