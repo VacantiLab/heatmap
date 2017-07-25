2fed56044e3ba7288002071979f88d95369e8a7d
Nathaniel Vacanti <VacantiLab@users.noreply.github.com>
2017-07-25 15:00:30 +0200

    allowed for errors in calculating the cox model because sometimes the gene expression information is not good enough

---
+++ GetCoxOutputs.R
@@ -0,0 +1,56 @@
+GetCoxOutputs <- function(CoxFormula,PatientInfoRequired,gene,CoxParameters,DataFrameIdentifier)
+# Sometimes the coxph will throw up an error because the data doesn't quite work
+#     In these cases the risk for that gene and that cancer-type is returned as 0 and the function continues
+{
+    #If there is no error thrown by coxph()
+    attempt <- try(CoxModel <- coxph(CoxFormula, data = PatientInfoRequired))
+    if (!("try-error" %in% class(attempt)))
+    {
+        CoxCoefficients <- CoxModel$coefficients
+        HazardModelCoefficients <- exp(CoxCoefficients) #This are the outputs of the model! Being one SD from the mean for the given measurement multiplies the hazard function by this coefficient
+        CoxCoefficientBounds <- confint(CoxModel)
+        #     2.5%   97.5%
+        #Age
+        #Gene
+
+        WaldCoxFitP <- 1-pchisq(CoxModel$wald.test,length(CoxModel$coefficients)) #the p for the whole model
+        CoxGeneP <- summary(CoxModel)$coefficients[gene,5] #The p for the gene coefficient
+
+        CoxCoefficientLowerBounds <- CoxCoefficientBounds[,1]
+        CoxCoefficientUpperBounds <- CoxCoefficientBounds[,2]
+        HazardModelCoefficientLowerBounds <- exp(CoxCoefficientLowerBounds)
+        HazardModelCoefficientUpperBounds <- exp(CoxCoefficientUpperBounds)
+
+        CoxResultsDataFrame <- data.frame(CoxParameters,HazardModelCoefficients,HazardModelCoefficientLowerBounds,HazardModelCoefficientUpperBounds)
+        CoxResultsDataFrame$CoxParameters <- factor(CoxResultsDataFrame$CoxParameters)
+        CoxResultsDataFrame$Identifier <- rep(DataFrameIdentifier,length(CoxResultsDataFrame[,1]))
+        #CoxResultsDataFrame$RiskLogRankP <- rep(RiskLogRankP,length(CoxResultsDataFrame[,1]))
+        CoxResultsDataFrame$CoxLikelihoodRatioWaldP <- rep(WaldCoxFitP,length(CoxResultsDataFrame[,1]))
+        CoxResultsDataFrame$CoxGeneP <- rep(CoxGeneP,length(CoxResultsDataFrame[,1]))
+        CoxResultsDataFrame$Risk <- NULL
+        if (is.na(CoxResultsDataFrame[gene,'HazardModelCoefficientLowerBounds'])){CoxResultsDataFrame[gene,'Risk'] <- 0}
+        if (!is.na(CoxResultsDataFrame[gene,'HazardModelCoefficientLowerBounds']))
+        {
+            if (CoxResultsDataFrame[gene,'HazardModelCoefficientLowerBounds'] < 1 && CoxResultsDataFrame[gene,'HazardModelCoefficientUpperBounds'] > 1){CoxResultsDataFrame[gene,'Risk'] <- 0}
+            if (CoxResultsDataFrame[gene,'HazardModelCoefficientLowerBounds'] > 1){CoxResultsDataFrame[gene,'Risk'] <- 1}
+            if (CoxResultsDataFrame[gene,'HazardModelCoefficientUpperBounds'] < 1){CoxResultsDataFrame[gene,'Risk'] <- -1}
+        }
+    }
+
+    #If there is an error thrown by coxph() return the risk as 0
+    #The other fields do not matter now because they are not considered in the analysis
+    if ("try-error" %in% class(attempt))
+    {
+        CoxResultsDataFrame <- data.frame(matrix(nrow=2,ncol=5))
+        rownames(CoxResultsDataFrame) <- c(gene,'Age')
+        colnames(CoxResultsDataFrame) <- c('CoxParameters','Identifier','CoxLikelihoodRatioWaldP','CoxGeneP','Risk')
+        CoxResultsDataFrame$CoxParameters <- c(0,0)
+        CoxResultsDataFrame$Identifier <- c(0,0)
+        #CoxResultsDataFrame$RiskLogRankP <- rep(RiskLogRankP,length(CoxResultsDataFrame[,1]))
+        CoxResultsDataFrame$CoxLikelihoodRatioWaldP <- c(0,0)
+        CoxResultsDataFrame$CoxGeneP <- c(0,0)
+        CoxResultsDataFrame$Risk <- c(0,0)
+    }
+
+    return(CoxResultsDataFrame)
+}
--- SurvivalAnalysis_support.R
+++ SurvivalAnalysis_support.R
@@ -18,7 +18,11 @@ CenterScale <- function(DataFrame,Columns,gene)
   ReturnDataFrame <- (DataFrame[Columns]-FactorMeanMatrix)/FactorSDMatrix
   return(ReturnDataFrame)
 }
+
+
 ##################################################################################################
+
+
 GetCox <- function(DataDirectory,PatientInfoFile,GeneExpressionFile,CoxParameters,DataFrameIdentifier,gene_array)
 {
     # QueryGenes is now defined below to loop through all genes
@@ -113,194 +117,26 @@ GetCox <- function(DataDirectory,PatientInfoFile,GeneExpressionFile,CoxParameter
         CoxParametersSumString <- paste(CoxParameters,collapse=' + ')
         CoxFormulaString <- paste('SurvObj ~ ',CoxParametersSumString)
         CoxFormula <- as.formula(CoxFormulaString)
-        CoxModel <- coxph(CoxFormula, data = PatientInfoRequired)
-        WaldCoxFitP <- 1-pchisq(CoxModel$wald.test,length(CoxModel$coefficients)) #the p for the whole model
-        CoxGeneP <- summary(CoxModel)$coefficients[gene,5] #The p for the gene coefficient
 
-        ##PI is the risk index
-        #CoxCoefficientMatrix <- t(replicate(NumPatients, CoxModel$coefficients))
-        #if (length(CoxParameters) > 1){PatientInfoRequired$PI <- rowSums(PatientInfoRequired[,CoxParameters]*CoxCoefficientMatrix)}
-        #if (length(CoxParameters) == 1){PatientInfoRequired$PI <- PatientInfoRequired[,CoxParameters]*CoxModel$coefficients[1]}
-        #PatientInfoRequired <- PatientInfoRequired[order(PatientInfoRequired[,'PI']),]
+        #Get the Cox Results for the gene and age
+        CoxResultsDataFrame <- GetCoxOutputs(CoxFormula,PatientInfoRequired,gene,CoxParameters,DataFrameIdentifier)
 
-        ##Divide into risk groups
-        #NumLowRisk <- ceiling(NumPatients/2)
-        #NumHighRisk <- NumPatients-NumLowRisk
-        #PatientInfoRequired$Risk <- 'Empty'
-        #PatientInfoRequired$Risk[1:NumLowRisk] <- 'Low Risk'
-        #PatientInfoRequired$Risk[(NumLowRisk+1):NumPatients] <- 'High Risk'
-        #PatientInfoRequired$CoxLikelihoodRatioWaldP <- rep(WaldCoxFitP,NumPatients)
-        #PatientInfoRequired$CoxGeneP <- rep(CoxGeneP,NumPatients)
-
-        ##Create the Kaplan Meier objects for plotting survival over time for risk groups
-        #KaplanMeierRisk <- survfit(SurvObj ~ Risk, data = PatientInfoRequired, conf.type = "log-log")
-        #KaplanMeierDiff <- survdiff(SurvObj ~ Risk, data = PatientInfoRequired)
-        #RiskLogRankP <- 1-pchisq(KaplanMeierDiff$chisq,length(KaplanMeierDiff$n)-1) #the p for the separate risk groups
-        #PatientInfoRequired$RiskLogRankP <- rep(RiskLogRankP,NumPatients)
-
-        #BoxPlotDataFrame <- PatientInfoRequired[c(CoxParameters,'Risk','CoxLikelihoodRatioWaldP','RiskLogRankP','CoxGeneP')]
-        #BoxPlotDataFrame <- BoxPlotDataFrame[complete.cases(BoxPlotDataFrame),]
-        #BoxPlotDataFrame <- gather_(BoxPlotDataFrame,'Factor','Measurement',CoxParameters)
-        #BoxPlotDataFrame$Identifier <- rep(DataFrameIdentifier,length(BoxPlotDataFrame[,1]))
-        #BoxPlotDataFrame$Risk <- factor(BoxPlotDataFrame$Risk, levels=c("Low Risk", "High Risk")) #sets the order for the boxplot
-
-        ##Create a merged data frame for low and high risk groups for plotting
-        #LowRiskIndicator <- rep('High Risk',KaplanMeierRisk$strata[1]) #I don't know what dermines the order, but based on the numbers in each group this is the correct assignment
-        #HighRiskIndicator <- rep('Low Risk',KaplanMeierRisk$strata[2])
-        #MergedTime <- KaplanMeierRisk$time
-        #MergedSurvival <- KaplanMeierRisk$surv
-        #MergedRiskIndicator <- c(LowRiskIndicator,HighRiskIndicator)
-        #SurvivalDataFrame <- data.frame(MergedTime,MergedSurvival,MergedRiskIndicator)
-        #SurvivalDataFrame$MergedRiskIndicator <- factor(SurvivalDataFrame$MergedRiskIndicator, levels=c("Low Risk", "High Risk")) #sets the order for the boxplot
-
-        #Get the model outputs
-        #A cox model is the hazard function multiplied by exp(CoxCoef*risk_factor), or H*exp(Cox*R)
-        #The Cox coefficient is the coefficient to the hazard function when R=1 or exp(Cox*1)
-        #In this model R is defined as standard deviation above the average for expression and the negative of age
-        CoxCoefficients <- CoxModel$coefficients
-        HazardModelCoefficients <- exp(CoxCoefficients) #This are the outputs of the model! Being one SD from the mean for the given measurement multiplies the hazard function by this coefficient
-        CoxCoefficientBounds <- confint(CoxModel)
-        #     2.5%   97.5%
-        #Age
-        #Gene
-        CoxCoefficientLowerBounds <- CoxCoefficientBounds[,1]
-        CoxCoefficientUpperBounds <- CoxCoefficientBounds[,2]
-        HazardModelCoefficientLowerBounds <- exp(CoxCoefficientLowerBounds)
-        HazardModelCoefficientUpperBounds <- exp(CoxCoefficientUpperBounds)
-
-        CoxResultsDataFrame <- data.frame(CoxParameters,HazardModelCoefficients,HazardModelCoefficientLowerBounds,HazardModelCoefficientUpperBounds)
-        CoxResultsDataFrame$CoxParameters <- factor(CoxResultsDataFrame$CoxParameters)
-        CoxResultsDataFrame$Identifier <- rep(DataFrameIdentifier,length(CoxResultsDataFrame[,1]))
-        #CoxResultsDataFrame$RiskLogRankP <- rep(RiskLogRankP,length(CoxResultsDataFrame[,1]))
-        CoxResultsDataFrame$CoxLikelihoodRatioWaldP <- rep(WaldCoxFitP,length(CoxResultsDataFrame[,1]))
-        CoxResultsDataFrame$CoxGeneP <- rep(CoxGeneP,length(CoxResultsDataFrame[,1]))
-        CoxResultsDataFrame$Risk <- NULL
-        if (is.na(CoxResultsDataFrame[gene,'HazardModelCoefficientLowerBounds'])){CoxResultsDataFrame[gene,'Risk'] <- 0}
-        if (!is.na(CoxResultsDataFrame[gene,'HazardModelCoefficientLowerBounds']))
-        {
-            if (CoxResultsDataFrame[gene,'HazardModelCoefficientLowerBounds'] < 1 && CoxResultsDataFrame[gene,'HazardModelCoefficientUpperBounds'] > 1){CoxResultsDataFrame[gene,'Risk'] <- 0}
-            if (CoxResultsDataFrame[gene,'HazardModelCoefficientLowerBounds'] > 1){CoxResultsDataFrame[gene,'Risk'] <- 1}
-            if (CoxResultsDataFrame[gene,'HazardModelCoefficientUpperBounds'] < 1){CoxResultsDataFrame[gene,'Risk'] <- -1}
-        }
-
-        #CoxResultsDataFrame['Age',] <- NULL
+        #Keep the gene results and discard the age results
         CoxResultsDataFrameList_gene[[gene]] <- CoxResultsDataFrame
         CoxResultsDataFrameList_gene[[gene]] <- CoxResultsDataFrameList_gene[[gene]][gene,]
 
+        #remove the gene information from the data frame passed to the CoxModel as it will be repopulated for another gene in the next loop
         PatientInfoRequired[,gene] <- NULL
     }
 
+    #Combine all Dataframes specific for genes by their rows to get a CoxResultsDataFrame for the cancer-type
     CoxResultsDataFrame <- do.call('rbind',CoxResultsDataFrameList_gene)
     CoxResultsDataFrame[,'CoxParameters'] <- NULL
     CoxResultsDataFrame[,'Identifier'] <- NULL
+
+    #Make a data frame to return that only has the risk information
     CoxResultsRisk <- CoxResultsDataFrame[,'Risk',drop=FALSE]
     colnames(CoxResultsRisk) <- DataFrameIdentifier
 
     return(CoxResultsRisk)
-}
-################################################################################################
-MakeBoxPlot_survival <- function(BoxPlotDataFrame,FileDirectory,FileName,x_var,y_var,color_var,YRangePlot,QueryGenes)
-{
-  FillColors <- c('#87CEFA','#6495ED')
-  names(FillColors) <- c('High Risk','Low Risk')
-  TextSize = 12
-  if (x_var=='Identifier') {YLabel = paste(QueryGenes[1],' Expression\n','(Standard Deviations From Mean)',sep='')}
-  else {YLabel = 'Standard Deviations From Mean'}
-
-  sp <- ggplot(BoxPlotDataFrame,aes_string(x=x_var, y=y_var)) +
-        #stat_boxplot(geom ='errorbar') +
-        geom_boxplot(aes_string(fill=color_var),outlier.colour='black',outlier.size=0,width=0.7,position=position_dodge(width=0.6)) +
-        #scale_y_continuous(breaks=YTicks, limits=YRangePlot, expand=c(0,0)) +
-        #coord_cartesian(ylim=c(-3,4)) +
-        #stat_summary(position=position_dodge(width=0.5)) +
-        #coord_cartesian(ylim=YRangePlot) +
-        theme(axis.text.y=element_text(color='black',size=TextSize)) +
-        theme(axis.ticks.y=element_line(colour='black',size=1)) +
-        theme(axis.ticks.x=element_line(colour='black',size=1)) +
-        theme(axis.text.x=element_text(color='black',size=TextSize,angle=90,hjust=1,vjust=0.5)) +
-        theme(axis.title.y=element_text(color='black',vjust=1,size=TextSize)) +
-        theme(axis.title.x=element_text(color='black',vjust=0,size=TextSize)) +
-        theme(panel.background=element_rect(fill=NA)) +
-        theme(axis.line=element_line(colour='black',size=1,linetype='solid')) +
-        theme(panel.grid.minor=element_blank(), panel.grid.major=element_blank()) + #removes gridlines
-        theme(legend.title=element_blank()) +
-        theme(legend.key=element_rect(fill=NA)) + #No background color in legend
-        theme(legend.text = element_text(colour="black", size=TextSize)) +
-        theme(legend.position=c(0.60,0.90)) +
-        scale_fill_manual(values=FillColors) +
-        #theme(legend.key.size = unit(0.2, "cm")) +
-        labs(x = '') +
-        labs(y =YLabel)
-        #aes_string() allows the factors to be specified by strings and ensures they are evaluated within the correct environment (aes() causes all sorts of trouble)
-
-  ggsave(paste(FileDirectory,FileName,sep=''), width = 12, height = 4, dpi = 120)
-  return()
-}
-########################################################################################################################################
-MakeSurvivalPlot <- function(SurvivalDataFrame,FileDirectory,FileName)
-{
-  FillColors <- c('#87CEFA','#6495ED')
-  names(FillColors) <- c('High Risk','Low Risk')
-  TextSize = 12
-
-  ggplot(SurvivalDataFrame,aes_string(x='MergedTime',y='MergedSurvival',group='MergedRiskIndicator')) +
-    geom_line(aes_string(colour='MergedRiskIndicator')) +
-    geom_point(aes_string(colour='MergedRiskIndicator'),size=3) +
-    scale_colour_manual(values=FillColors) +
-    scale_y_continuous(limits=c(0.2,1),labels=scales::percent,expand=c(0,0)) + #expand=c(0,0) ensures there is no space between data and axis
-    scale_x_continuous(limits=c(0,4000),breaks=c(0,1000,2000,3000,4000),expand=c(0,0)) +
-    theme(axis.text.y=element_text(color='black',size=TextSize)) +
-    theme(axis.ticks.y=element_line(colour='black',size=1)) +
-    theme(axis.ticks.x=element_line(colour='black',size=1)) +
-    theme(axis.text.x=element_text(color='black',size=TextSize)) +
-    theme(axis.title.y=element_text(color='black',vjust=1,size=TextSize)) +
-    theme(axis.title.x=element_text(color='black',vjust=0,size=TextSize)) +
-    theme(panel.background=element_rect(fill=NA)) +
-    theme(axis.line=element_line(colour='black',size=1,linetype='solid')) +
-    theme(panel.grid.minor=element_blank(), panel.grid.major=element_blank()) + #removes gridlines
-    theme(legend.title=element_blank()) +
-    theme(legend.key=element_rect(fill=NA)) + #No background color in legend
-    theme(legend.text = element_text(colour="black", size=TextSize)) +
-    theme(legend.position=c(0.8,0.85)) +
-    labs(x = 'Days') +
-    labs(y ='Percent Surviving')
-
-  ggsave(paste(FileDirectory,FileName,sep=''), width = 4, height = 4, dpi = 120)
-}
-###############################################################################################################
-MakeSurvivalCoefficientPlot <- function(CoxResultsDataFrame,FileDirectory,FileName,x_var,color_var)
-{
-  FillColors <- c('#87CEFA','#6495ED')
-  TextSize = 12
-
-  ggplot(CoxResultsDataFrame, aes_string(x=x_var,y='HazardModelCoefficients',fill=color_var)) +
-    geom_bar(position=position_dodge(),stat='identity',color='black') +
-    geom_errorbar(aes_string(ymax='HazardModelCoefficientUpperBounds',ymin='HazardModelCoefficientLowerBounds'),
-                  position=position_dodge(0.9),width=0.2,col='black') +
-    geom_hline(yintercept=1,linetype=2) +
-    #scale_y_continuous(expand=c(0,0),limits=c(0,4.0)) +
-    coord_cartesian(ylim=c(0,7)) +
-    theme(axis.text.y=element_text(color='black',size=TextSize)) +
-    theme(axis.ticks.y=element_line(colour='black',size=1)) +
-    theme(axis.ticks.x=element_line(colour='black',size=1)) +
-    theme(axis.text.x=element_text(color='black',size=TextSize,angle=90,hjust=1,vjust=0.5)) +
-    theme(axis.title.y=element_text(color='black',vjust=1,size=TextSize)) +
-    theme(axis.title.x=element_text(color='black',vjust=0,size=TextSize)) +
-    theme(panel.background=element_rect(fill=NA)) +
-    theme(axis.line=element_line(colour='black',size=1,linetype='solid')) +
-    theme(panel.grid.minor=element_blank(), panel.grid.major=element_blank()) + #removes gridlines
-    theme(legend.title=element_blank()) +
-    theme(legend.key=element_rect(fill=NA)) + #No background color in legend
-    theme(legend.text = element_text(colour="black", size=TextSize)) +
-    theme(legend.position=c(0.40,0.80)) +
-    theme(legend.direction='horizontal') +
-    scale_fill_manual(values=FillColors) +
-    guides(fill = guide_legend(override.aes = list(colour = NULL))) + #removes diagonal line from legend entries
-    theme(legend.key = element_rect(colour = 'black')) + #adds border to legened entries
-    #theme(legend.key.size = unit(0.2, "cm")) +
-    labs(x = '') +
-    labs(y ='Hazard Model Coefficient')
-
-  ggsave(paste(FileDirectory,FileName,sep=''), width = 12.0, height = 4, dpi = 120)
-
 }
