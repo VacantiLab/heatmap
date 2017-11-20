e5a52fbe440cfa1f359f0a9cba965fe00a3f6549
Nathaniel Vacanti <VacantiLab@users.noreply.github.com>
2017-11-20 15:35:36 +0100

    boxplot now returns the data transformed as plotted and transformed if all groups were considered

--- MakeBoxPlot.R
+++ MakeBoxPlot.R
@@ -35,6 +35,7 @@ MakeBoxPlot <- function(data_location,ColGroupsScheme=NULL,transformation=NULL,d
     gene_name <- ArrangeData_return[[4]]
     DATA_long <- ArrangeData_return[[5]]
     FillColors <- ArrangeData_return[[6]]
+    DATA_transformed <- ArrangeData_return[[7]]
     DATA_transformed_full <- ArrangeData_return[[10]]
 
     #Find the y-limits for the boxplot based on the data
@@ -44,7 +45,7 @@ MakeBoxPlot <- function(data_location,ColGroupsScheme=NULL,transformation=NULL,d
     b <- assemble_box_plot(DATA_long,FillColors,output_directory,y_bounds)
 
     #assemble variables to return
-    MakeBoxPlot_return <- list(sig_test_list,DATA_transformed_full)
+    MakeBoxPlot_return <- list(sig_test_list,DATA_transformed,DATA_transformed_full)
 
     return(MakeBoxPlot_return)
 }
