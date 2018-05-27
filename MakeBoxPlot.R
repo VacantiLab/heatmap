49fa119a93ac65639ae46269ea631129bf03c8d4
Nathaniel Vacanti <nathaniel.vacanti@gmail.com>
2018-05-27 12:55:13 +0200

    made a default value of FALSE for qc_plot in MakeBoxPlot

--- MakeBoxPlot.R
+++ MakeBoxPlot.R
@@ -1,4 +1,4 @@
-MakeBoxPlot <- function(data_location,ColGroupsScheme=NULL,transformation=NULL,data=NULL,select_rows=NULL,select_groups=NULL,replicate_scheme=NULL,qc_plot)
+MakeBoxPlot <- function(data_location,ColGroupsScheme=NULL,transformation=NULL,data=NULL,select_rows=NULL,select_groups=NULL,replicate_scheme=NULL,qc_plot=FALSE)
 # data_location: a pathway to where the text file containing the data is stored, must have '/' at the end
 #    The data file must be named quantities.txt with the genes down the rows and sample names across the columns
 #    There must also be a group_key.txt file with the sample names down the rows and the grouping schemes across the columns
