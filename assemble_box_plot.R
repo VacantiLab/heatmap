7d5471517b53a1f44889a1cfae2c8d814c0fe50f
Nathaniel Vacanti <Nate@dhcp-ccc-3208.eduroam.cornell.edu>
2018-09-07 16:55:50 -0400

    changed the width of boxplot and the viewing of individual points to off

--- assemble_box_plot.R
+++ assemble_box_plot.R
@@ -20,7 +20,7 @@ assemble_box_plot <- function(DATA_long,FillColors,output_directory,y_bounds,qc_
     XLabel <- ''
     TextSize = 8
 
-    pdf_width <- unit(4,'cm')
+    pdf_width <- unit(2,'cm')
     pdf_height <- unit(4,'cm')
     bar_width <- 0.50
     inter_group_spacing <- 0.55
@@ -43,7 +43,7 @@ assemble_box_plot <- function(DATA_long,FillColors,output_directory,y_bounds,qc_
     }
 
     #If you want to add markers for points on the boxplot
-    boxplot_points <- TRUE #for now this needs to be changed in the source code to have points on the boxplot
+    boxplot_points <- FALSE #for now this needs to be changed in the source code to have points on the boxplot
     gtp <- NULL
     if(boxplot_points)
     {
