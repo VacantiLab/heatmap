d8bb55ffec852e04d736af43e71c9ab7ecbd1687
Nate <nate@dhcp-ccc-10253.redrover.cornell.edu>
2019-02-22 18:16:15 -0500

    added the capability to make point plots with error bars as if they are boxplots

--- assemble_box_plot.R
+++ assemble_box_plot.R
@@ -16,22 +16,32 @@ assemble_box_plot <- function(DATA_long,FillColors,output_directory,y_bounds,qc_
 
     no_groups_color <- 'grey'
 
-    YLabel <- 'Abundance'
+    YLabel <- 'Relative Abundance'
     XLabel <- ''
     TextSize = 8
 
-    pdf_width <- unit(6,'cm')
-    pdf_height <- unit(4,'cm')
-    bar_width <- 0.50
-    inter_group_spacing <- 0.55
-    legend_position <- c(0.12,0.98)
+    pdf_width <- unit(3,'in')
+    pdf_height <- unit(2,'in')
+    bar_width <- 0.30
+    inter_group_spacing <- 0.40
+    legend_position <- c(0.10,0.92)
 
     #If there are groups, the group color is specified by the color_var
     if(!is.null(FillColors))
     {
-        gbp <- geom_boxplot(aes_string(fill=color_var),outlier.colour='black',outlier.size=0.5,width=bar_width,position=position_dodge(width=inter_group_spacing),outlier.shape=NA,lwd=0.2)
+        #gbp <- geom_boxplot(aes_string(fill=color_var),outlier.colour='black',outlier.size=0.5,width=bar_width,position=position_dodge(width=inter_group_spacing),outlier.shape=NA,lwd=0.2)
+
+        #If you want labeled outliers
         #gbp <- geom_boxplot(aes_string(fill=color_var),outlier.colour='black',outlier.size=0.5,width=bar_width,position=position_dodge(width=inter_group_spacing),outlier.shape=20,lwd=0.2)
-            # second line provides outlier shape
+
+        #If you want a scatter plot with mean and SD
+        library(plyr) #this package contains the ddply function which allows for making a data frame with summary statistics
+        DATA_long_summary <- ddply(DATA_long,c(color_var,x_var),summarise,value2=mean(value),sd=sd(value))
+        #  if you use 'value' to name the column instead of 'value2', the standard deviations will not calculate
+        colnames(DATA_long_summary)[colnames(DATA_long_summary)=='value2']='value'
+        #  putting the name, 'value', back as the column name
+        gbp <- geom_errorbar(aes(ymin=value-sd,ymax=value+sd,color=group),width=bar_width,position=position_dodge(width=inter_group_spacing))
+
     }
 
     #If there are no groups, the group color is specified by no_groups_color
@@ -43,26 +53,35 @@ assemble_box_plot <- function(DATA_long,FillColors,output_directory,y_bounds,qc_
     }
 
     #If you want to add markers for points on the boxplot
-    boxplot_points <- FALSE #for now this needs to be changed in the source code to have points on the boxplot
+    boxplot_points <- TRUE #for now this needs to be changed in the source code to have points on the boxplot
     gtp <- NULL
     if(boxplot_points)
     {
-        gtp <- geom_text(aes_string(label='sample',group=color_var),size=2,position=position_dodge(width=inter_group_spacing))
+        #If you want to include all points
+        #gtp <- geom_text(aes_string(label='sample',group=color_var),size=2,position=position_dodge(width=inter_group_spacing))
+
+        #If you want the point to be the mean - the points are colored
+        gtp <- geom_point(aes(color=group),position=position_dodge(width=inter_group_spacing),size=2.5)
     }
 
     #set y-bounds manually if desired
-    #y_bounds <- c(-1,1)
+    manual_ybounds <- TRUE
+    if(manual_ybounds){y_bounds <- c(-2,3)}
 
+    scatter_box <- TRUE
+    if(scatter_box){DATA_to_plot <- DATA_long_summary}
+    if(!scatter_box){DATA_to_plot <- DATA_long}
 
-
-    b <- ggplot(DATA_long,aes_string(x=x_var, y=y_var)) +
+    b <- ggplot(DATA_to_plot,aes_string(x=x_var, y=y_var)) +
          #gvp +
          gbp +
          gtp +
          theme(axis.text.y=element_text(color='black',size=TextSize)) +
          theme(axis.ticks.y=element_line(colour='black',size=0.5)) +
          theme(axis.ticks.x=element_line(colour='black',size=0.5)) +
-         theme(axis.text.x=element_text(color='black',size=TextSize,angle=90,hjust=1,vjust=0.5)) +
+         #Set the x-axis text
+         #theme(axis.text.x=element_text(color='black',size=TextSize,angle=90,hjust=1,vjust=0.5)) +
+         theme(axis.text.x=element_text(color='black',size=TextSize,angle=0,vjust=0.5)) +
          theme(axis.title.y=element_text(color='black',vjust=1,size=TextSize)) +
          theme(axis.title.x=element_text(color='black',vjust=0,size=TextSize)) +
          theme(panel.background=element_rect(fill=NA)) +
@@ -75,6 +94,7 @@ assemble_box_plot <- function(DATA_long,FillColors,output_directory,y_bounds,qc_
          theme(legend.text = element_text(colour="black", size=(TextSize-2))) +
          theme(legend.position=legend_position) +
          scale_fill_manual(values=FillColors) +
+         scale_color_manual(values=FillColors) +
          #theme(legend.key.size = unit(0.2, "cm")) +
          labs(x = XLabel) +
          labs(y = YLabel) +
